89c4615b1029a008580eb97f8f085cfe05c316bd
duong4431 <duong4431@gmail.com>
2020-06-01 14:54:25 +0700

    bai tap transition

---
+++ vuetransition/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Vue Transition demo</h2>
+    <button @click="showMess = !showMess">Show/Hide Content</button>
+    <transition name="mess">
+      <div class="message" v-show="showMess">
+        <span>Đăng ký thành công!</span>
+      </div>
+    </transition>
+  </div>  
+</template>
+<script>
+export default {
+  data(){
+    return {
+      showMess:false
+    }
+  }
+}
+</script>
+<style scoped>
+.message{
+  position:fixed;
+  right:10px;
+  bottom:10px;
+  padding:20px;
+  background-color:rgba(10, 136, 31, 0.6);
+  border:1px solid rgba(0,0,0,0.1);
+  border-radius:3px;
+  color:white;
+}
+.mess-leave-to,
+.mess-enter{
+  opacity:0;
+}
+.mess-leave,
+.mess-enter-to{
+  opacity:1;
+}
+.mess-leave-active,
+.mess-enter-active{
+  transition:opacity 0.5s;
+}
+</style>
---
+++ vuetransition/src/components/mobile.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="wrapper">
+        <div class="header">
+            <div class="topnav">
+                <span>Chào mừng bạn đến với</span>
+            </div>
+            <div class="nav">
+                <div class="menu">
+                    <span class="btnMenu"></span>
+                    <ul>
+                        <li><a href="#">Trang chủ</a></li>
+                        <li><a href="#">Sản phẩm</a></li>
+                        <li><a href="#">Hướng dẫn đặt hàng</a></li>
+                        <li><a href="#">Giới thiệu</a></li>
+                        <li><a href="#">Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div class="logo">
+                    <h1>NinhBinhWeb</h1>
+                    <span>Sản phẩm bảo vệ sức khỏe</span>
+                </div>
+            </div>
+            <div class="taskbar">
+                <div class="all-pro-cate">
+                    <span class="btnAllProCate"></span>
+                    <ul>
+                        <li><a href="#">Máy lọc nước</a></li>
+                        <li><a href="#">Máy lọc nước</a></li>
+                        <li><a href="#">Máy lọc nước</a></li>
+                        <li><a href="#">Máy lọc nước</a></li>
+                    </ul>
+                </div>
+                <div class="search-box">
+                    <input type="text" placeholder="Nhập sản phẩm bạn cần tìm...">
+                </div>
+            </div>
+        </div> 
+        <div class="content">
+            <div class="slide">
+                <div class="slide-img">
+                    <img src="" alt="">
+                </div>
+                <div class="slide-item">
+                    <span>Máy lọc nước giảm giá 40%</span>
+                    <span>Máy lọc nước giảm giá 40%</span>
+                    <span>Máy lọc nước giảm giá 40%</span>
+                </div>
+            </div>
+            <div class="popular-cate">
+                <span>Máy lọc khí dân dụng</span>
+                <span>Máy lọc khí công nghiệp</span>
+                <span>Máy hút ẩm</span>
+                <span>Máy tạo ẩm</span>
+                <span>Linh kiện - phụ kiện</span>
+                <span>Máy quét</span>
+            </div>
+        </div>   
+        <div class="footer">
+            <span>Gọi điện</span>
+            <span>Nhắn tin</span>
+            <span>Chat zalo</span>
+            <span>Chat facebook</span>
+        </div>
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
